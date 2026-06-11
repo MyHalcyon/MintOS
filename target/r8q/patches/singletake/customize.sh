4a1b42eab4f684015ef2fe2464d6186b312660ff
Z3phery <dz.ignacioaguilar@gmail.com>
2026-06-10 23:04:17 -0400

    r8q: patches: adjustments in single take patch
    Signed-off-by: Z3phery <dz.ignacioaguilar@gmail.com>

--- target/r8q/patches/singletake/customize.sh
+++ target/r8q/patches/singletake/customize.sh
@@ -1,7 +1,5 @@
-PATCHED=false
-
 # Single Take fix (pre-API 35)
-if [ "$TARGET_PLATFORM_SDK_VERSION" -lt "35" ]; then
+if [ "$TARGET_API_LEVEL" -lt "35" ]; then
     if [ ! -d "$WORK_DIR/vendor/etc/singletake/ClarityScorer" ]; then
         PATCHED=true
         if [ -d "$WORK_DIR/vendor/etc/singletake/aifilter" ]; then
@@ -14,9 +12,3 @@ if [ "$TARGET_PLATFORM_SDK_VERSION" -lt "35" ]; then
             "etc/singletake/ClarityScorer" 0 2000 755 "u:object_r:vendor_configs_file:s0"
     fi
 fi
-
-if ! $PATCHED; then
-    LOG "\033[0;33m! Nothing to do\033[0m"
-fi
-
-unset PATCHED
